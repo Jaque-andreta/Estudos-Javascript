0232f6e9db03bc3989c26e15ec433a5b6274a47f
Jaqueline Andreta <115796795+Jaque-andreta@users.noreply.github.com>
2024-03-27 16:30:05 -0300

    Add files via upload

---
+++ Desafios/Dia da semana.js
@@ -0,0 +1,6 @@
+var diaDaSemana = prompt("Olá, como vai? Qual é o dia da semana hoje?");
+if (diaDaSemana == "sábado" || diaDaSemana == "domingo") {
+  alert("Bom final de semana!");
+} else {
+  alert("Boa semana!");
+}
